e1a0ef75b28458c43dd25a640ec69f24814db803
gourabb820 <91730250+gourabb820@users.noreply.github.com>
2023-07-07 16:14:51 +0530

    modified date format

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 import "./App.css";
-import ResultList from "./components/ResutlList";
+import ResultList from "./components/result/ResutlList";
+import NavBar from "./components/navbar/NavBar";
 
 function App() {
   const [result, setResult] = useState([]);
@@ -10,23 +11,20 @@ function App() {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          'https://dlofp4oo16.execute-api.ap-south-1.amazonaws.com/default/fetch-count'
+          "https://vi3vquu9ff.execute-api.ap-south-1.amazonaws.com/default/FetchLetterCountFromRDS"
         );
-        // const jsonData = await response.json();
-        debugger;
         setResult(response && response.data && response.data.data);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
-
     fetchData();
   }, []);
 
   return (
     <div className="App">
-      {result.length ?  <ResultList list={result} /> : null}
-      
+      <NavBar title={"Record from RDS and fetched using Lambda Service"} />
+      {result.length ? <ResultList list={result} /> : null}
     </div>
   );
 }
--- src/components/result/ResutlList.js
+++ src/components/result/ResutlList.js
@@ -1,9 +1,21 @@
 import "./Result.css";
 
 function ResultList({ list }) {
+
+  let sl = 1;
+
+  //Format Date time in read able format
+  function formatDateTime(timestamp){   
+    const date = new Date(timestamp);  
+    const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric', hour: 'numeric', minute: 'numeric' };
+    const formattedDate = date.toLocaleString('en-IN', options);
+    return formattedDate
+  }
+
   return (
     <div className="list">
       <div className="list_header">
+        <h3 className="list_item">SL No</h3>
         <h3 className="list_item">File Name</h3>
         <h3 className="list_item">Letter Count</h3>
         <h3 className="list_item">Created Date</h3>
@@ -12,9 +24,10 @@ function ResultList({ list }) {
         ? list.map((r) => {
             return (
               <div className="list_body">
+                <h4 className="list_item">{sl++}</h4>
                 <h4 className="list_item">{r.FILE_NAME}</h4>
                 <h4 className="list_item">{r.CHAR_COUNT}</h4>
-                <h4 className="list_item">{r.CREATED_DATE}</h4>
+                <h4 className="list_item">{formatDateTime(r.CREATED_DATE)}</h4>
               </div>
             );
           })
